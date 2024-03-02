fa9d07ec67a929236b618667e84ee5a9f6853510
shubashdutta <shubashdutta6@gmail.com>
2024-03-02 16:47:45 +0545

    first commit

--- src/comp/Myproject.jsx
+++ src/comp/Myproject.jsx
@@ -9,7 +9,7 @@ function Myproject() {
     </div>
 
     <div className='md:flex md:flex-wrap justify-center md:justify-center w-100%  md:h-100% md:w-100% h-100%'>
-        <div className=' w-[80%]  shadow-2xl   ml-11  items-center md:blur-[2px] hover:blur-none  h-[435px] md:w-[23%] rounded-xl  mb-5 md:h-[434px] border-2'>
+        <div className=' w-[80%]  shadow-2xl   ml-11  items-center md:blur-[2px] hover:blur-none  h-[439px] md:w-[23%] rounded-xl  mb-5 md:h-[439px] border-2'>
             <img className=' rounded-lg  h-[200px] w-full' src="./Screenshot.png" alt="projectimg" />
             <div className='bg-white  mt-[3px]'>
                 <h3 className='text-center text-lg capitalize  font-semibold'>saibaba || ngo web_site</h3>
@@ -17,7 +17,7 @@ function Myproject() {
             </div>
             <a target='blank' href="https://github.com/shubashdutta"><button className=' md:text-2xl text-lg md:ml-20 mt-1 ml-20 hover:bg-green-800 rounded-lg bg-indigo-900 text-white  border-2'>socre code</button></a>
         </div>
-        <div className='w-[80%] ml-11 shadow-lg  items-center md:blur-[2px] hover:blur-none mb-5    h-[425px] md:w-[23%] rounded-xl md:h-[430px] border-2 ' >
+        <div className='w-[80%] ml-11 shadow-lg  items-center md:blur-[2px] hover:blur-none mb-5    h-[425px] md:w-[23%] rounded-xl md:h-[429px] border-2 ' >
         <img className=' rounded-lg  h-[200px] w-full' src="./prime.png" alt="projectimg" />
             <div className='bg-white  mt-[3px]'>
                 <h3 className='text-center text-lg capitalize  font-semibold'>Primestar || consulting</h3>
@@ -27,7 +27,7 @@ function Myproject() {
             </div>
             <a target='blank' href="https://github.com/shubashdutta"><button className=' md:text-2xl text-lg md:ml-[77px] mt-1 ml-20 hover:bg-green-800 rounded-lg bg-indigo-900 text-white   border-2'>socre code</button></a>
         </div>
-        <div className='w-[80%]  ml-11 shadow-lg md:blur-[2px] hover:blur-none  items-center  h-[435px] md:w-[23%] rounded-xl md:h-[449px] border-2'>
+        <div className='w-[80%]  ml-11 shadow-lg md:blur-[2px] hover:blur-none  items-center  h-[435px] md:w-[23%] rounded-xl md:h-[448px] border-2'>
         <img className=' rounded-lg  h-[200px] w-full' src="./e-com.png" alt="projectimg" />
             <div className='bg-white  mt-[3px]'>
                 <h3 className='text-center text-md capitalize  font-semibold'>Returngiftnow || E-commerce</h3>
