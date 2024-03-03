1035a2f9d4632a67dc847eca221724f549cf39a3
shubashdutta <shubashdutta6@gmail.com>
2024-03-03 09:04:26 +0545

    first commit

--- src/comp/Cont.jsx
+++ src/comp/Cont.jsx
@@ -9,24 +9,24 @@ import subash from "../comp/subash.pdf";
 function Cont() {
   return (
     <>
-      <div className="  bg-white w-full">
+      <div className=" mt-5 bg-white w-full">
         <h2 className="text-center text-3xl  py-8 capitalize ">contact me</h2>
       </div>
 
       <div className="w-full flex    h-48 justify-center gap-1 bg-white">
         <div className="mt-5 w-[25%] h-[70%] md:w-[15%] shadow-md border-4">
-          <center className="animate-bounce text-xl md:text-2xl md:w-[25%] hover:bg-green-900 md:ml-20  mt-5 justify-center p-2  w-[40%] ml-6 rounded-[50%]  text-white bg-indigo-600 ">
+          <center className="animate-bounce text-md md:text-2xl md:w-[25%] hover:bg-green-900 md:ml-20  mt-5 justify-center p-2  w-[40%] ml-6 rounded-[50%]  text-white bg-indigo-600 ">
             <FontAwesomeIcon icon={faEnvelope} />
           </center>
-          <center className="mt-5 mr-5 md:mr-2 hover:text-green-500 text-md text-3xl font-semibold">
+          <center className="mt-5 mr-5 md:mr-2 hover:text-green-500 text-sm md:text-3xl font-semibold">
             <a href="mailto:subashdutta6@gmail.com">Email</a>
           </center>
         </div>
         <div className="mt-5 w-[20%] h-[70%] md:w-[15%] shadow-md border-4">
-          <center className="animate-bounce text-xl md:text-2xl md:w-[25%] hover:bg-green-900 md:ml-20  mt-5 justify-center p-2  w-[20%] ml-6 rounded-[50%]  text-white bg-indigo-600 ">
+          <center className="animate-bounce text-md md:text-2xl md:w-[25%] hover:bg-green-900 md:ml-20  mt-5 justify-center p-2  w-[40%] ml-6 rounded-[50%]  text-white bg-indigo-600 ">
             <FontAwesomeIcon icon={faLinkedin} />
           </center>
-          <center className="mt-5 mr-5 md:mr-2 hover:text-green-500 text-md md:text-3xl font-semibold">
+          <center className="mt-5 mr-5 md:mr-2 hover:text-green-500 text-sm md:text-3xl font-semibold">
             <a
               target="blank"
               href="https://www.linkedin.com/in/shubash-dutta-98a843271/"
@@ -39,17 +39,17 @@ function Cont() {
           <center className=" animate-bounce text-md md:text-2xl md:w-[25%] hover:bg-green-900 md:ml-20  mt-5 justify-center p-2  w-[40%] ml-6 rounded-[50%]  text-white bg-indigo-600 ">
             <FontAwesomeIcon icon={faGithub} />
           </center>
-          <center className="mt-5 mr-5 md:mr-2 hover:text-green-500 text-md md:text-3xl font-semibold">
+          <center className="mt-5 mr-5 md:mr-2 hover:text-green-500 text-sm md:text-3xl font-semibold">
             <a target="blank" href="https://github.com/shubashdutta">
               Github
             </a>
           </center>
         </div>
         <div className="mt-5 w-[20%] h-[70%] md:w-[15%] shadow-md border-4">
-          <center className="animate-bounce text-xl md:text-2xl md:w-[25%] hover:bg-green-900 md:ml-20  mt-5 justify-center p-2  w-[40%] ml-6 rounded-[50%]  text-white bg-indigo-600 ">
+          <center className="animate-bounce text-md md:text-2xl md:w-[25%] hover:bg-green-900 md:ml-20  mt-5 justify-center p-2  w-[40%] ml-6 rounded-[50%]  text-white bg-indigo-600 ">
             <FontAwesomeIcon icon={faFile} />
           </center>
-          <center className="mt-5 mr-5 md:mr-2 hover:text-green-500 text-md md:text-2xl  font-bold">
+          <center className="mt-5 mr-5 md:mr-2 hover:text-green-500 text-sm md:text-2xl  font-semibold md:font-bold">
             <a href={subash} download="subash">
               Download Cv
             </a>
